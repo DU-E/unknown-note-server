4a27a0c8ce7f2b0fcf49a1e8774ff2b05ce40fc4
곰돌씨 <64863514+tony7078@users.noreply.github.com>
2024-03-09 01:39:59 +0900

    v0.2 동규 통합 버전

---
+++ unknown-note-server/src/main/java/unknownnote/unknownnoteserver/UnknownnoteserverApplication.java
@@ -0,0 +1,14 @@
+package unknownnote.unknownnoteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class UnknownnoteserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnknownnoteserverApplication.class, args);
+	}
+
+}
